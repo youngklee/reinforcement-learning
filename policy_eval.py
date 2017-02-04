0d99750a984eb2acd70e48dd6f590b1a5c588fe1
Young Lee <ylee@example.com>
2017-02-03 23:58:15 -0600

    discount_factor >> gamma

--- policy_eval.py
+++ policy_eval.py
@@ -4,9 +4,7 @@ if '../' not in sys.path:
     sys.path.append('../')
 from lib.envs.gridworld import GridworldEnv
 
-env = GridworldEnv()
-
-def policy_eval(policy, env, discount_factor=1.0, theta=0.00001):
+def policy_eval(policy, env, gamma=1.0, theta=0.00001):
     V = np.zeros(env.nS)
     nS = env.nS
     P = env.P
@@ -16,13 +14,15 @@ def policy_eval(policy, env, discount_factor=1.0, theta=0.00001):
             v = 0
             for a, action_prob in enumerate(policy[s]):
                 for prob, ns, reward, _ in env.P[s][a]:
-                    v += action_prob*prob*(reward + discount_factor*V[ns])
+                    v += action_prob*prob*(reward + gamma*V[ns])
             delta = max(delta, np.abs(v - V[s]))
             V[s] = v
         if delta < theta:
             break
     return np.array(V)
 
-random_policy = np.ones([env.nS, env.nA])/env.nA
-v = policy_eval(random_policy, env)
-print('v: {}'.format(v))
+if __name__ == '__main__':
+    env = GridworldEnv()
+    random_policy = np.ones([env.nS, env.nA])/env.nA
+    v = policy_eval(random_policy, env)
+    print('v: {}'.format(v))
