fe76c5cc3fb8b1fbc138761661ca658af2c1e0b7
Young Lee <ylee@example.com>
2017-02-03 23:58:35 -0600

    Add policy iteration

---
+++ policy_iteration.py
@@ -0,0 +1,32 @@
+import numpy as np
+from policy_eval import policy_eval
+from lib.envs.gridworld import GridworldEnv
+
+def policy_improvement(policy, env, policy_eval_fn=policy_eval, gamma=1.0):
+    while True:
+        V = policy_eval_fn(policy, env, gamma)
+        stable = True
+        for s in range(env.nS):
+            best_action = np.argmax(policy[s])
+            action_values = np.zeros(env.nA)
+            for a in range(env.nA):
+                for prob, ns, reward, _ in env.P[s][a]:
+                    action_values[a] += prob*(reward + gamma*V[ns])
+                new_best_action = np.argmax(action_values)
+            if not best_action == new_best_action:
+                stable = False
+            policy[s] = np.eye(env.nA)[new_best_action]
+
+        if stable:
+            return policy, V
+
+if __name__ == '__main__':
+    env = GridworldEnv()
+    random_policy = np.ones([env.nS, env.nA])/env.nA
+    policy, v = policy_improvement(random_policy, env)
+    print('Policy:\n{}'.format(policy))
+    print('Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):\n{}'.format(
+        np.reshape(np.argmax(policy, axis=1), env.shape)))
+    print('Value Function:\n{}'.format(v))
+    print('Reshaped Grud Value Function:\n{}'.format(
+        v.reshape(env.shape)))
